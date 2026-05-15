2ccb4b6e183079f3705c44f47071da728898d47b
Fiona June Leathers <fiona.june.leathers@outlook.com>
2026-05-15 16:24:04 -0400

    refactor to use ResourceGraphClient

--- CloudHarvestPluginAzure/credentials.py
+++ CloudHarvestPluginAzure/credentials.py
@@ -5,13 +5,24 @@ from azure.mgmt.resource import ResourceManagementClient
 
 
 class CachedSubscriptions:
+    # We effectively create three indexes of information for quick lookup: subscriptions, resource groups,
+    # and resource groups by location.
     subscriptions = {}
+    resource_groups = {}
+    resource_groups_by_location = {}
+
     refresh_thread = None
 
     @staticmethod
-    def get_all_subscriptions() -> dict:
-        # Generates an index of Azure subscriptions by id and their associated resource groups.
-        results = {}
+    def refresh_all_subscriptions():
+        """
+        Generates an index of subscriptions, resource groups, and locations.
+        :return: None
+        """
+    
+        by_subscription_results = {}
+        by_resource_group_results = {}
+        by_resource_group_location_results = {}
 
         # Generate a list of subscriptions using the local credentials
         # IDE note: DefaultAzureCredential() is sometimes highlighted as being an incompatible credential for the
@@ -22,31 +33,58 @@ class CachedSubscriptions:
         # Iterate over the subscriptions to collect their configurations and resource group information.
         for subscription in subscriptions:
             resource_groups = [
-                group for group in
-                ResourceManagementClient(subscription.credential, subscription.subscription_id).resource_groups.list()
+                group.as_dict() for group in
+                ResourceManagementClient(credential=DefaultAzureCredential(), subscription_id=subscription.subscription_id).resource_groups.list()
             ]
 
-            results.update(
+            # Record the results for this subscription by subscription id.
+            by_subscription_results.update(
                 {
-                    subscription.subscription_id: {
-                        "subscription": subscription,
-                        "resource_groups": {
-                            group.name: group
-                            for group in resource_groups
-                        },
-                    }
+                    subscription.subscription_id:
+                        {
+                            'subscription': subscription,
+                            'resource_groups':
+                                {
+                                    group['id']: group
+                                    for group in resource_groups
+                                }
+                        }
                 }
             )
 
-        CachedSubscriptions.subscriptions = results
+            # Record the results for this subscription by resource group identifier.
+            by_resource_group_results.update(
+                {
+                    group['id']:
+                        {
+                            'group': group,
+                            'subscription': subscription,
+                        }
+                    for group in resource_groups
+                }
+            )
+
+            # Record resource groups by region (location) code.
+            for group in resource_groups:
+                gl = group['location']
+                if gl not in by_resource_group_location_results.keys():
+                    by_resource_group_location_results[gl] = {}
+
+                by_resource_group_location_results[gl][group['id']] = {
+                    group['id']: group | {'subscription': subscription}
+                }
+
+        # Update the class variables with the collected results.
+        CachedSubscriptions.subscriptions = by_subscription_results
+        CachedSubscriptions.resource_groups = by_resource_group_results
+        CachedSubscriptions.by_group_location_results = by_resource_group_location_results
+
         from datetime import datetime
         CachedSubscriptions.last_collected = datetime.now()
 
         if CachedSubscriptions.refresh_thread is None:
             from threading import Thread
-            CachedSubscriptions.refresh_thread = Thread(target=CachedSubscriptions.get_all_subscriptions, daemon=True)
-
-        return results
+            CachedSubscriptions.refresh_thread = Thread(target=CachedSubscriptions.refresh_all_subscriptions, daemon=True)
 
     @staticmethod
     def get_subscription(subscription_id: str) -> Subscription | None:
@@ -66,5 +104,5 @@ class CachedSubscriptions:
         from time import sleep
 
         while True:
-            CachedSubscriptions.get_all_subscriptions()
+            CachedSubscriptions.refresh_all_subscriptions()
             sleep(600)  # Sleep for 10 minutes before refreshing again
--- CloudHarvestPluginAzure/tasks/azure.py
+++ CloudHarvestPluginAzure/tasks/azure.py
@@ -1,22 +1,19 @@
 from CloudHarvestCoreTasks.dataset import WalkableDict
 from CloudHarvestCoreTasks.tasks import BaseTask
 from CloudHarvestCorePluginManager.decorators import register_definition
+from azure.mgmt.resourcegraph.models import QueryRequest
 
 
 @register_definition(name='azure', category='task')
 class AzureTask(BaseTask):
 
     def __init__(self,
-                 command: str,
-                 arguments: dict = None,
-                 client_arguments: dict = None,
                  service: str = None,
                  type: str = None,
                  account: str = None,
                  region: str = None,
-                 paginator_method: str = 'as_dict',
                  include_metadata: bool = True,
-                 global_service: bool = False,
+                 # global_service: bool = False,
                  max_retries: int = 10,
                  result_path: str or list or tuple = None,
                  *args,
@@ -25,40 +22,29 @@ class AzureTask(BaseTask):
         Constructs all the necessary attributes for the AzureTask object.
 
         Args:
-            command (str): The package and command path to execute for the service.
-            arguments (dict): The arguments to pass to the final command. Defaults to empty dictionary.
-            client_arguments (dict, optional): Arguments to pass to the command's client class.
             service (str, optional): The Azure service to interact with (e.g., 's3', 'ec2'). If not specified, the default is pulled from the task chain variables.
             type (str, optional): The type of the Azure service (e.g., 's3', 'ec2'). If not specified, the default is pulled from the task chain variables.
             account (str, optional): The Azure number to use for the session. If not specified, the default is pulled from the task chain variables.
             region (str, optional): The Azure region to use for the session. None is supported as not all Azure services require a region. If not specified, the default is pulled from the task chain variables.
-            paginator_method (str, optional): The method to use to paginate results. Default is 'as_dict'.
             include_metadata (bool, optional): When True, some 'Harvest' metadata fields are added to the result. Defaults to True.
-            global_service (bool, optional): If True, the service is considered a global service (e.g., IAM). Negates the 'region' input. Defaults to False.
             max_retries (int, optional): The maximum number of retries for the command. Defaults to 10.
-            result_path (str, optional): Path to the results. When not provided, the path is the response itself.
         """
 
         # Initialize parent class
         super().__init__(*args, **kwargs)
 
-        # Set STAR: Service, Type, Account, Region, (Resource) Group
+        # Set STAR: Service, Type, Account (Subscription), Region (Resource) Group
         self.service = service or self.task_chain.variables.get('service')
         self.type = type or self.task_chain.variables.get('type')
         self.account = str(account or self.task_chain.variables.get('account'))     # Azure "subscription_id"
-        self.region = None if global_service else region or self.task_chain.variables.get('region')     # Azure "location"
-        self.group = None  # Azure resource_group
-        self.paginator_method = paginator_method or 'as_dict'
+        self.region = region or self.task_chain.variables.get('region')     # Azure "location"
 
         # azure sdk configuration
-        self.command = command
-        self.arguments = arguments or {}
-        self.client_arguments = client_arguments or {}
         self.max_retries = max_retries
 
         # Output manipulation
         self.include_metadata = include_metadata
-        self.result_path = result_path
+        self.result_path = result_path or 'data'
 
         # Programmatic attributes
         self.account_alias = None
@@ -85,13 +71,11 @@ class AzureTask(BaseTask):
 
         # Execute the Azure query
         result = query_azure(
-            subscription=self.account,
-            command=self.command,
-            arguments=self.arguments,
-            client_arguments=self.client_arguments,
+            service=self.service,
+            service_type=self.type,
+            account=self.account,
+            region=self.region,
             max_retries=self.max_retries,
-            result_path=self.result_path,
-            paginator_method=self.paginator_method
         )
 
         # Add starting metadata to the result
@@ -118,23 +102,20 @@ class AzureTask(BaseTask):
 
 
 def query_azure(
-        subscription: str,
-        command: str,
-        arguments: dict,
-        client_arguments: dict,
-        paginator_method: str,
+        service: str,
+        service_type: str,
+        account: str,
+        region: str,
         max_retries: int,
-        result_path
-) -> WalkableDict:
+) -> list:
     """
     Queries Azure for the specified service and command.
 
     Arguments
-        subscription (str): The Azure subscription_id to query.
-        command (str): The command to execute on the Azure service.
-        arguments (dict): The arguments to pass to the command.
-        client_arguments (dict): The arguments to pass to the command's client class.
-        paginator_method (str): The method to use to paginate results. Default is 'as_dict'.
+        service (str): The Azure subscription_id to query.
+        service_type (str): The Azure resource type to query.
+        account (str): The Azure subscription_id to query.
+        region (str): The Azure location to query
         credentials (dict, optional): The Azure credentials to use for the session.
         max_retries (int, optional): The maximum number of retries for the command. Defaults to 10.
         result_path (str, optional): Path to the results. When not provided, the path is the object itself.
@@ -142,53 +123,17 @@ def query_azure(
     Returns:
         Any: The result of the Azure query.
     """
+    from azure.identity import DefaultAzureCredential
+    from azure.mgmt.resourcegraph import ResourceGraphClient
+    from azure.mgmt.resourcegraph.models import QueryRequest, QueryRequestOptions, QueryResponse
     max_retries = max_retries or 10
 
     # Initialize the result and attempt counter
     attempt = 0
 
-    # Verify that the `command` is formatted correctly
-    from re import match
-    if not match(r"^[a-zA-Z0-9._-]+:[a-zA-Z0-9._-]+$", command):
-        raise ValueError('azure `command` must be in the format "package:ClientClass.method1.method2.methodn" (e.g., "azure.mgmt.postgresqlflexibleservers:PostgreSQLManagementClient.servers.list_by_resource_group") format.')
-
-    # `command` is a loaded argument. It requires the following syntax: 'package:ClassName.path.to.method' where
-    # `arguments` will be fed to the final method in the path. This is required because many Azure commands are nested
-    # such as: `azure.mgmt.postgresqlflexibleservers:PostgreSQLManagementClient().servers.list_by_resource_group()`.
-    command_package, command_class = command.split(':')
-    command_methods = command_package.split('.')[1:]
-
-    # Dynamically import the Azure client's module
-    try:
-        from importlib import import_module
-        azure_module = import_module(command_package)
-
-    except ModuleNotFoundError as e:
-        raise ModuleNotFoundError(f'`{command_package}` could not be loaded.') from e
-
-    # Check that the client class exists
-    if not hasattr(azure_module, command_class):
-        raise Exception(f'`{command_package}`.{command_class} does not exist.')
-
-    # The client object allows connection to the Azure API.
-    from CloudHarvestPluginAzure.credentials import CachedSubscriptions
-    # Add the credentials to the client arguments, overriding 'credentials' if provided
-    client_arguments.update({'credentials': CachedSubscriptions.get_subscription(subscription)})
-
-    # Instantiate the client object with the provided arguments.
-    client = getattr(azure_module, command_class)(**client_arguments)
-
-    # Set and verify the client's method chain using functools.reduce which walks the entire class_methods chain and
-    # returns the last method in sequence.
-    try:
-        from functools import reduce
-        command_method = reduce(getattr, command_methods, client)
-
-    except Exception as e:
-        raise Exception(f'Could not find a method in {command_package}:{command_class}.{".".join(command_methods)}.') from e
-
     # Initialize the result dictionary.
-    result = {}
+    result = []
+    client = ResourceGraphClient(DefaultAzureCredential())
 
     # Start a loop to execute the command
     while True:
@@ -200,24 +145,28 @@ def query_azure(
             if attempt > max_retries:
                 raise Exception('Max retries exceeded')
 
-            # Execute the command. Unlike boto3, Azure SDK documentation claims that pagination is handled automatically
-            # by the client, so we don't need to check for pagination here; however, the automated pagination is managed
-            # through the act of iterating over the response. Therefore, we encapsulate the getattr() operation in a
-            # list comprehension with the expectations that a valid response will paginate.
-            # https://learn.microsoft.com/en-us/azure/developer/python/sdk/fundamentals/common-types-response
+            # Pagination loop
+            skip_token = None
+            while True:
+                # Build the query
+                query = QueryRequest(
+                    query=f"resources | where"
+                          f" type == 'microsoft.{service}/{service_type}'"
+                          f" location == '{region}'"
+                          f" subscriptionId == '{account}'",
+                    options=QueryRequestOptions(
+                        skip_token=skip_token,
+                    )
+                )
 
-            # The paginator_method is sometimes required to convert the Azure responses into useful data. For example,
-            # the PostgreSQLManagementClient().servers.list_by_resource_group() method returns a class that requires
-            # the as_dict() method to derive responses in JSON.
-            if paginator_method:
-                result = [
-                    getattr(r, paginator_method)() for r in command_method(**arguments)
-                ]
+                pagination_result = client.resources(query).as_dict()
+                result.append(pagination_result.get('data'))
 
-            else:
-                result = [
-                    r for r in command_method(**arguments)
-                ]
+                if pagination_result.get('skip_token'):
+                    skip_token = pagination_result.get('skip_token')
+
+                else:
+                    break
 
             # If no errors occurred during processing, we can assume that (even an empty) result is a success.
             break
@@ -233,17 +182,25 @@ def query_azure(
             else:
                 raise e from e
 
-    result = WalkableDict(result)
+    # Determine the 'data' type of all responses. We expect that 'data' is always a list of dictionaries.
+    result_types = list(set(type(r) for r in result))
+    if len(result_types) == 1:
+        if result_types[0] == list:
+            result = [
+                item
+                for sublist in result
+                for item in sublist
+            ]
 
-    # If a result key is specified, extract the result using the key. If no result key was specified, then return
-    # the object as-is (no-op)
-    if isinstance(result_path, str):
-        result = result.walk(result_path)
+        elif result_types[0] == dict:
+            result = {
+                key: value
+                for r in result
+                for key, value in r.items()
+            }
+            # Convert the result to a list because that's what the calling method expects
+            result = [result]
+    else:
+        raise Exception(f'Inconsistent result type: {result_types}')
 
-    elif isinstance(result_path, (list, tuple)):
-        result = {
-            path: result.walk(path)
-            for path in result_path
-        }
-
-    return result if isinstance(result, WalkableDict) and result is not None else WalkableDict(result)
+    return result
